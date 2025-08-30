7b09ce860b520e7d78bb709597949b1096bae617
sivasai <145899497+Sivsai@users.noreply.github.com>
2025-08-30 10:48:09 +0530

    Update AppConstants.kt

--- FlashFeed/app/src/main/java/com/littlelemon/appsnews/data/AppConstants.kt
+++ FlashFeed/app/src/main/java/com/littlelemon/appsnews/data/AppConstants.kt
@@ -3,5 +3,6 @@ package com.littlelemon.appsnews.data
 object AppConstants {
     const val APP_BASE_URL = "https://newsapi.org/"
 const val COUNTRY ="us"
-    const val API_KEY = "aa07924db5644705b8fcbc04a43c7474"
+    const val API_KEY = "YOUR_API_KEY"
+
 }
